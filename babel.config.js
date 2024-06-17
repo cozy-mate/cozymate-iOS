fae4e6a870111a8878603ed3db6b9bb9e63ce364
JJHyeok13 <jjinh99@naver.com>
2024-06-17 18:42:10 +0900

    feat: babel native-wind 및 alias 설정

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,37 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugin: [
+    // native-wind 설정
+    ['nativewind/babel'],
+    // alias 설정
+    [
+      'module-resolver',
+      {
+        root: ['.'],
+        extensions: [
+          '.js',
+          '.jsx',
+          '.ts',
+          '.tsx',
+          '.android.js',
+          '.android.tsx',
+          '.ios.js',
+          '.ios.tsx',
+        ],
+        alias: {
+          '@providers': './src/providers',
+          '@components': './src/components',
+          '@hooks': './src/hooks',
+          '@pages': './src/pages',
+          '@routes': './src/routes',
+          '@type': './src/type',
+          '@utils': './src/utils',
+          '@assets': './src/assets',
+          '@recoil': './src/recoil',
+          '@server': './src/server',
+          '@axios': './src/axios',
+        },
+      },
+    ],
+  ],
 };
