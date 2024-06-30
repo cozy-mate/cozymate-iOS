6f9a8693c2bf3a9e8e2f176d73d4bc0c21788661
JJHyeok13 <jjinh99@naver.com>
2024-06-30 19:21:55 +0900

    feat: todoBox Pressable 추가 및 todoList 스타일 수정

--- src/components/todoList/todoBox.tsx
+++ src/components/todoList/todoBox.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Pressable, Text, View } from 'react-native';
+
+import TodoBoxIcon from '@assets/todoList/todoBoxIcon.svg';
+import DoneTodoBoxIcon from '@assets/todoList/doneTodoBoxIcon.svg';
 
 interface TodoBoxProps {
   todoData: {
@@ -11,12 +14,22 @@ interface TodoBoxProps {
 
 const TodoBox: React.FC<TodoBoxProps> = ({ todoData }) => {
   return (
-    <View className="p-2 pl-4 bg-white shadow-chipback">
+    <View className="p-2 pl-4 bg-white shadow-chipback rounded-xl">
       {todoData ? (
         <>
           {todoData.map((data) => (
-            <View key={data.index}>
-              <Text>{data.name}</Text>
+            <View
+              key={data.index}
+              className="flex flex-row justify-between items-center mb-1 py-[11.5px]"
+            >
+              <Text
+                className={`${data.isDone ? 'text-disabledFont line-through' : 'text-basicFont'}`}
+              >
+                {data.name}
+              </Text>
+              <Pressable className="p-[6px]">
+                {data.isDone ? <DoneTodoBoxIcon /> : <TodoBoxIcon />}
+              </Pressable>
             </View>
           ))}
         </>
--- src/components/todoList/todoList.tsx
+++ src/components/todoList/todoList.tsx
@@ -13,12 +13,14 @@ interface TodoBoxProps {
 const TodoList: React.FC<TodoBoxProps> = ({ todoData }) => {
   return (
     <View>
-      <Text className="text-base font-semibold text-emphasizedFont">
-        오늘 <Text className="text-main">델로</Text>님이
-      </Text>
-      <Text className="text-base font-semibold text-emphasizedFont">
-        해야할 일들을 알려드릴게요!
-      </Text>
+      <View className="mb-4">
+        <Text className="text-base font-semibold text-emphasizedFont">
+          오늘 <Text className="text-main">델로</Text>님이
+        </Text>
+        <Text className="text-base font-semibold text-emphasizedFont">
+          해야할 일들을 알려드릴게요!
+        </Text>
+      </View>
       <TodoBox todoData={todoData} />
     </View>
   );
