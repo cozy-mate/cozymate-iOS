678d693ce934c78783d628b36dd1637bde84e907
JJHyeok13 <jjinh99@naver.com>
2024-06-30 17:10:27 +0900

    feat: 투두리스트 스크린 stack 추가

--- src/type/param/stack.ts
+++ src/type/param/stack.ts
@@ -2,20 +2,30 @@ import { NativeStackScreenProps } from '@react-navigation/native-stack';
 
 export type RootStackParamList = {
   SignInScreen: undefined;
+  OnBoardScreen: undefined;
   HomeScreen: undefined;
   RoomMainScreen: undefined;
-  OnBoardScreen: undefined;
-  RoomMateScreen: undefined;
   CreateRoomScreen: undefined;
+  TodoListScreen: undefined;
+
+  RoomMateScreen: undefined;
   UserDetailScreen: undefined;
   UserInfoScreen: undefined;
 };
 
+// 시작 스크린
 export type SignInScreenProps = NativeStackScreenProps<RootStackParamList, 'SignInScreen'>;
+// 온보딩 스크린
 export type OnBoardScreenProps = NativeStackScreenProps<RootStackParamList, 'OnBoardScreen'>;
+// 홈 스크린 (방 만들기 Or 초대코드 입력하기)
 export type HomeScreenProps = NativeStackScreenProps<RootStackParamList, 'HomeScreen'>;
+// 방 메인 스크린 (코지봇 알림)
 export type RoomMainScreenProps = NativeStackScreenProps<RootStackParamList, 'RoomMainScreen'>;
-export type RoomMateScreenProps = NativeStackScreenProps<RootStackParamList, 'RoomMateScreen'>;
+// 방장 방 만들기 스크린 (방 이름 & 인원 선택)
 export type CreateRoomScreenProps = NativeStackScreenProps<RootStackParamList, 'CreateRoomScreen'>;
+// 투두리스트 스크린
+export type TodoListScreenProps = NativeStackScreenProps<RootStackParamList, 'TodoListScreen'>;
+
+export type RoomMateScreenProps = NativeStackScreenProps<RootStackParamList, 'RoomMateScreen'>;
 export type UserDetailScreenProps = NativeStackScreenProps<RootStackParamList, 'UserDetailScreen'>;
 export type UserInfoScreenProps = NativeStackScreenProps<RootStackParamList, 'UserInfoScreen'>;
