19cac5fedf126f109a4856e625090f95efcff9ac
JJHyeok13 <jjinh99@naver.com>
2024-06-30 18:50:04 +0900

    feat: Todolist, RoleRule 컴포넌트 생성

---
+++ src/components/todoList/roleRule.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const RoleRule: React.FC = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+};
+
+export default RoleRule;
