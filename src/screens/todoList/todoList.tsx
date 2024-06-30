bcb88e1ab434864b193dfdcc8f4b6570628e66e0
JJHyeok13 <jjinh99@naver.com>
2024-06-30 17:17:28 +0900

    feat: 투두리스트 스크린 navbar 생성

--- src/screens/todoList/todoList.tsx
+++ src/screens/todoList/todoList.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import { Pressable, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native';
 
+import Background from '@assets/background.svg';
+
 import { TodoListScreenProps } from '@type/param/stack';
 
 const TodoListScreen = ({ navigation }: TodoListScreenProps) => {
   return (
-    <SafeAreaView className="flex-1">
+    <View className="flex-1 bg-[#CADFFF]">
+      <Background />
       <View className="flex-1 mx-6"></View>
-    </SafeAreaView>
+    </View>
   );
 };
 
