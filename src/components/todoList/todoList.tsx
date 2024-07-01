8dab7b2653c819a265dce32b9bf7e06b83779e74
JJHyeok13 <jjinh99@naver.com>
2024-07-01 11:48:42 +0900

    feat: todoList 텍스트 수정

--- src/components/todoList/todoList.tsx
+++ src/components/todoList/todoList.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Text, View } from 'react-native';
 import TodoBox from './todoBox';
 
+import SettingIcon from '@assets/todoList/settingIcon.svg';
+
 interface TodoBoxProps {
   todoData: {
     index: number;
@@ -13,13 +15,16 @@ interface TodoBoxProps {
 const TodoList: React.FC<TodoBoxProps> = ({ todoData }) => {
   return (
     <View>
-      <View className="mb-4">
-        <Text className="text-base font-semibold text-emphasizedFont">
-          오늘 <Text className="text-main">델로</Text>님이
-        </Text>
-        <Text className="text-base font-semibold text-emphasizedFont">
-          해야할 일들을 알려드릴게요!
-        </Text>
+      <View className="flex flex-row items-center justify-between mb-4">
+        <View>
+          <Text className="text-base font-semibold text-emphasizedFont">
+            6/30 (일), <Text className="text-main">델로</Text>님이
+          </Text>
+          <Text className="text-base font-semibold text-emphasizedFont">
+            해야할 일들을 알려드릴게요!
+          </Text>
+        </View>
+        <SettingIcon />
       </View>
       <TodoBox todoData={todoData} />
     </View>
