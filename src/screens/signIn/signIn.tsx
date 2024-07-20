aba47d44808b9bc015ab61dcdcb6b1e88dfa4d63
JJHyeok13 <jjinh99@naver.com>
2024-07-20 21:57:56 +0900

    feat: signin 스크린 필요없는 네비게이션 제거(온보딩, 코지홈만 남김)

--- src/screens/signIn/signIn.tsx
+++ src/screens/signIn/signIn.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { Pressable, Text, View } from 'react-native';
-import { SafeAreaView } from 'react-native';
+import { Pressable, Text, View, SafeAreaView } from 'react-native';
 
 import KakaoLogo from '@assets/signIn/kakaoLogo.svg';
 import AppleLogo from '@assets/signIn/appleLogo.svg';
+
 import { SignInScreenProps } from '@type/param/stack';
 
 const SignInScreen = ({ navigation }: SignInScreenProps) => {
@@ -15,32 +15,12 @@ const SignInScreen = ({ navigation }: SignInScreenProps) => {
     navigation.navigate('CozyHomeScreen');
   };
 
-  const toRoomMate = () => {
-    navigation.navigate('RoomMateScreen');
-  };
-
-  const toCreateRoom = () => {
-    navigation.navigate('CreateRoomScreen');
-  };
-
-  const toUserDetail = () => {
-    navigation.navigate('UserDetailScreen');
-  };
-
-  const toTodoList = () => {
-    navigation.navigate('TodoListScreen');
-  };
-
-  const toUserInfoScreen = () => {
-    navigation.navigate('UserInfoScreen');
-  };
-
   return (
     <SafeAreaView className="flex-1 bg-white">
       <View className="flex-1 mx-6">
-        <View className="items-center justify-center flex-1">
-          <View className="flex-row mb-[7px]">
-            <Text className="text-[42px] font-extrabold">
+        <View className="mx-auto mt-[236px] mb-[224px]">
+          <View className="mb-[7px]">
+            <Text className="text-[42px] font-extrabold text-center">
               <Text className="text-[#FFE28B]">cozy</Text>
               <Text className="text-[#BDD8FF]">mate</Text>
             </Text>
@@ -56,65 +36,19 @@ const SignInScreen = ({ navigation }: SignInScreenProps) => {
             onPress={toOnBoard}
           >
             <KakaoLogo className="mr-2" />
-            <Text className="font-semibold text-black opacity-85">온보딩 스크린</Text>
+            <Text className="text-base font-semibold text-black opacity-85">
+              카카오톡으로 계속하기
+            </Text>
           </Pressable>
         </View>
 
         <View className="mx-3 mb-4">
           <Pressable
-            className="flex-row justify-center items-center rounded-[33px] bg-navergreen px-6 py-4"
+            className="flex-row items-center justify-center rounded-[33px] bg-appleblack px-4 py-[14px]"
             onPress={toCozyHome}
           >
-            <Text className="font-semibold text-black opacity-85">홈 스크린</Text>
-          </Pressable>
-        </View>
-
-        <View className="mx-3 mb-4">
-          <Pressable
-            className="flex-row justify-center items-center rounded-[33px] bg-navergreen px-6 py-4"
-            onPress={toRoomMate}
-          >
-            <Text className="font-semibold text-black opacity-85">룸메이트 스크린</Text>
-          </Pressable>
-        </View>
-
-        <View className="mx-3 mb-4">
-          <Pressable
-            className="flex-row items-center rounded-[33px] bg-appleblack px-[96px] py-[14px]"
-            onPress={toCreateRoom}
-          >
-            <AppleLogo className="mr-2" />
-            <Text className="font-semibold text-center text-white">방 생성하기 스크린</Text>
-          </Pressable>
-        </View>
-
-        <View className="mx-3 mb-4">
-          <Pressable
-            className="flex-row items-center rounded-[33px] bg-appleblack px-[96px] py-[14px]"
-            onPress={toTodoList}
-          >
-            <AppleLogo className="mr-2" />
-            <Text className="font-semibold text-center text-white">투두리스트 스크린</Text>
-          </Pressable>
-        </View>
-
-        <View className="mx-3 mb-4">
-          <Pressable
-            className="flex-row items-center rounded-[33px] bg-appleblack px-[96px] py-[14px]"
-            onPress={toUserDetail}
-          >
-            <AppleLogo className="mr-2" />
-            <Text className="font-semibold text-center text-white">상세 스크린</Text>
-          </Pressable>
-        </View>
-
-        <View className="mx-3 mb-11">
-          <Pressable
-            className="flex-row items-center rounded-[33px] bg-appleblack px-[96px] py-[14px]"
-            onPress={toUserInfoScreen}
-          >
-            <AppleLogo className="mr-2" />
-            <Text className="font-semibold text-center text-white">상세 스크린</Text>
+            <AppleLogo className="mr-4" />
+            <Text className="text-base font-semibold text-center text-white">Apple로 계속하기</Text>
           </Pressable>
         </View>
       </View>
