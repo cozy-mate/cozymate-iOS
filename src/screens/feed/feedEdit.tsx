64585f29b4e12ae0bd77f304127d718e40e2d73b
jpark0506 <peterp1@inha.edu>
2024-08-07 17:58:57 +0900

    [COZY-157] feat: 피드 편집 뷰 완료

--- src/screens/feed/feedEdit.tsx
+++ src/screens/feed/feedEdit.tsx
@@ -1,15 +1,58 @@
-import React from 'react'
+import React, {useState,useEffect} from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
-import { View } from 'react-native'
+import { View,TextInput,Text,Pressable } from 'react-native'
 
 type Props = {}
 
 const FeedEditScreen = (props: Props) => {
+
+  const [feedName, setFeedName] = useState<string>('');
+  const [feedDescription, setFeedDescription] = useState<string>('');
+
+  const [isComplete, setIsComplete] = useState<boolean>(false);
+  //TODO: useEffect를 사용하여 불러올 정보
+  // - 피드 이름, 설명 가져오기
+
+  //TODO: useCheckValidation이라는 custom hook을 만들어서 사용해도 좋을 것 같습니다.
+
+  useEffect(() => {
+    if (feedName !== '' && feedDescription !== '') {
+      setIsComplete(true);
+    }else{
+      setIsComplete(false);
+    }
+  }, [feedName, feedDescription]);
+
+  const valueHandleNameChange = (text: string) => {
+    setFeedName(text);
+  };
+  const valueHandleDescriptionChange = (text: string) => {
+    setFeedDescription(text);
+  };
+
   return (
     <SafeAreaView className="flex-1 flex-col bg-white pl-8 pr-8 pt-8 w-full h-full">
-      <View>
-
+      <View className='flex-1 flex-col'>
+        <Text className='text-md mb-2 font-semibold text-emphasizedFont'>피드 이름을 입력해주세요.</Text>
+        <TextInput
+            placeholder="내용를 입력해주세요"
+            value={feedName}
+            onChangeText={valueHandleNameChange}
+            className="w-full p-4 pr-8 bg-colorBox text-basicFont rounded-xl mb-12"
+          />
+        <Text className='text-md mb-2 font-semibold text-emphasizedFont'>피드 설명을 입력해주세요.</Text>
+        <TextInput
+            placeholder="내용를 입력해주세요"
+            value={feedDescription}
+            onChangeText={valueHandleDescriptionChange}
+            className="w-full p-4 pr-8 bg-colorBox text-basicFont rounded-xl"
+          />
       </View>
+      <View className="flex">
+          <Pressable className={`${isComplete ? 'bg-main1' : 'bg-[#C4C4C4]'}  p-4 rounded-xl`}>
+            <Text className="text-base font-semibold text-center text-white">확인</Text>
+          </Pressable>
+        </View>
     </SafeAreaView>
   )
 }
