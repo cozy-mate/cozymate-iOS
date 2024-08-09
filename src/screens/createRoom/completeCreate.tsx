bc720fb35572ac53ad07f7d2f77c68c9ea24a177
JJHyeok13 <jjinh99@naver.com>
2024-08-10 00:42:34 +0900

    feat: 방 생성 완료 스크린 구현 완료

--- src/screens/createRoom/completeCreate.tsx
+++ src/screens/createRoom/completeCreate.tsx
@@ -13,35 +13,39 @@ const CompleteCreateRoomScreen = ({ navigation }: CompleteCreateRoomScreenProps)
   };
 
   return (
-    <SafeAreaView className="flex-1">
-      <View className="flex flex-col justify-between">
-        <View className="flex flex-col">
-          <Text className="text-xl font-semibold text-emphasizedFont">방 생성을 완료했어요!</Text>
-          <Text className="text-sm font-medium text-basicFont">
-            초대코드를 공유해 룸메이트를 모아보세요..
-          </Text>
-        </View>
-
-        <Pressable className="flex">
-          <View className="px-6 py-3 rounded-xl bg-colorBox">
-            <Text className="text-main1">QUIIRK</Text>
+    <SafeAreaView className="flex flex-col justify-between flex-1 bg-white">
+      <View className="flex flex-1 flex-col justify-between mt-[81px] px-5">
+        <View className="flex items-center">
+          <View className="flex flex-col mb-5">
+            <Text className="mb-1 text-xl font-semibold text-center text-emphasizedFont">
+              방 생성을 완료했어요!
+            </Text>
+            <Text className="text-sm font-medium text-center text-basicFont">
+              초대코드를 공유해 룸메이트를 모아보세요..
+            </Text>
           </View>
-        </Pressable>
 
-        <View>
-          <Image
-            source={{
-              uri: `https://staging-cozymate-s3.s3.ap-northeast-2.amazonaws.com/persona/png/${createroomState.profileImage}.png`,
-            }}
-            style={{ width: 300, height: 300 }}
-            resizeMode="cover"
-          />
+          <Pressable className="flex mb-16">
+            <View className="px-6 py-3 rounded-xl bg-colorBox">
+              <Text className="text-base font-semibold text-main1">QUIIRK</Text>
+            </View>
+          </Pressable>
+
+          <View className="flex">
+            <Image
+              source={{
+                uri: `https://staging-cozymate-s3.s3.ap-northeast-2.amazonaws.com/persona/png/${createroomState.profileImage}.png`,
+              }}
+              style={{ width: 300, height: 300 }}
+              resizeMode="cover"
+            />
+          </View>
         </View>
 
         <View className="flex">
           <Pressable onPress={toCozyHome}>
             <View className="p-4 rounded-xl bg-main1">
-              <Text className="text-base font-semibold text-center text-white">완료</Text>
+              <Text className="text-base font-semibold text-center text-white">확인</Text>
             </View>
           </Pressable>
         </View>
