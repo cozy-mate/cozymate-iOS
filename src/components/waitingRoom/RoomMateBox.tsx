96c83a70a623ce2dbf2c939ca33ba9a5e99fd917
JJHyeok13 <jjinh99@naver.com>
2024-08-12 04:47:11 +0900

    feat: 룸메이트 대기 스크린 완성

--- src/components/waitingRoom/RoomMateBox.tsx
+++ src/components/waitingRoom/RoomMateBox.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Image, Text, View } from 'react-native';
 
+import EmptyCharacter from '@assets/waitingRoom/emptyCharacter.svg';
+
 interface RoomMateBoxProps {
   name: string;
   profileImage: number;
@@ -13,16 +15,25 @@ const RoomMateBox: React.FC<RoomMateBoxProps> = ({ name, profileImage, state, is
     <View
       className={`${
         state === '도착완료' ? 'bg-[#e4efff]' : 'bg-box'
-      } flex flex-col items-center p-5 pt-4 rounded-xl`}
+      } flex flex-col items-center p-5 pt-4 rounded-xl mb-9 min-w-[159px]`}
     >
-      <Image
-        source={{
-          uri: `https://staging-cozymate-s3.s3.ap-northeast-2.amazonaws.com/persona/png/${profileImage}.png`,
-        }}
-        style={{ width: 100, height: 100 }}
-        resizeMode="cover"
-      />
-      <Text className="mt-3 mb-[2px] text-sm font-semibold text-main1">
+      {profileImage == 0 ? (
+        <EmptyCharacter />
+      ) : (
+        <Image
+          source={{
+            uri: `https://staging-cozymate-s3.s3.ap-northeast-2.amazonaws.com/persona/png/${profileImage}.png`,
+          }}
+          style={{ width: 100, height: 100 }}
+          resizeMode="cover"
+        />
+      )}
+
+      <Text
+        className={`${
+          name != '???' ? 'text-main1' : 'text-disabledFont'
+        } mt-3 mb-[2px] text-sm font-semibold  `}
+      >
         {name}
         {isAdmin ? '(방장)' : ''}
       </Text>
--- src/screens/waitingRoom/waitingRoom.tsx
+++ src/screens/waitingRoom/waitingRoom.tsx
@@ -1,6 +1,6 @@
 import { WaitingRoomScreenProps } from '@type/param/loginStack';
 import React, { useState } from 'react';
-import { Image, SafeAreaView, Text, View } from 'react-native';
+import { Image, Pressable, SafeAreaView, ScrollView, Text, View } from 'react-native';
 
 import XButton from '@assets/xButton.svg';
 import HomeIcon from '@assets/waitingRoom/homeIcon.svg';
@@ -22,7 +22,31 @@ const WaitingRoomScreen = ({ navigation }: WaitingRoomScreenProps) => {
       state: '도착완료',
       isAdmin: true,
     },
+    {
+      name: '포비',
+      profileImage: 2,
+      state: '도착완료',
+      isAdmin: false,
+    },
+    {
+      name: '눈꽃',
+      profileImage: 3,
+      state: '도착완료',
+      isAdmin: false,
+    },
   ]);
+
+  // 총 인원 수보다 멤버리스트가 적다면 추가
+  const fullMemberList = [...memberList];
+
+  while (fullMemberList.length < roomInfo.numOfMate) {
+    fullMemberList.push({
+      name: '???',
+      profileImage: 0,
+      state: '대기중',
+      isAdmin: false,
+    });
+  }
 
   const toMain = () => {
     navigation.navigate('MainScreen');
@@ -30,61 +54,74 @@ const WaitingRoomScreen = ({ navigation }: WaitingRoomScreenProps) => {
 
   return (
     <SafeAreaView className="flex-1 bg-white">
-      <View className="flex flex-col justify-between flex-1 px-5">
-        <View className="flex">
-          <View className="flex flex-row justify-end mt-2 mb-8">
-            <XButton />
-          </View>
+      <ScrollView>
+        <View className="flex flex-col flex-1 px-5">
+          <View className="flex">
+            <View className="flex flex-row justify-end mt-2 mb-8">
+              <Pressable onPress={toMain}>
+                <XButton />
+              </Pressable>
+            </View>
 
-          <View className="flex flex-col items-center mb-10">
-            <Text className="text-xl font-semibold text-emphasizedFont">
-              cozymate가 모이고 있어요!
-            </Text>
-            <Text className="text-sm font-medium text-disabledFont">
-              모든 cozymate가 모이면 방이 열려요..
-            </Text>
-          </View>
+            <View className="flex flex-col items-center mb-10">
+              <Text className="text-xl font-semibold text-emphasizedFont">
+                cozymate가 모이고 있어요!
+              </Text>
+              <Text className="text-sm font-medium text-disabledFont">
+                모든 cozymate가 모이면 방이 열려요..
+              </Text>
+            </View>
 
-          <View className="flex flex-col items-center mb-8">
-            <Image
-              source={{
-                uri: `https://staging-cozymate-s3.s3.ap-northeast-2.amazonaws.com/persona/png/${roomInfo.profileImage}.png`,
-              }}
-              style={{ width: 120, height: 120 }}
-              resizeMode="cover"
-            />
-            <View className="flex flex-row mt-3">
-              <View className="flex flex-row items-center">
-                <HomeIcon />
-                <Text className="ml-1 text-xs font-medium text-basicFont">{roomInfo.roomName}</Text>
-              </View>
+            <View className="flex flex-col items-center mb-8">
+              <Image
+                source={{
+                  uri: `https://staging-cozymate-s3.s3.ap-northeast-2.amazonaws.com/persona/png/${roomInfo.profileImage}.png`,
+                }}
+                style={{ width: 120, height: 120 }}
+                resizeMode="cover"
+              />
+              <View className="flex flex-row mt-3">
+                <View className="flex flex-row items-center">
+                  <HomeIcon />
+                  <Text className="ml-1 text-xs font-medium text-basicFont">
+                    {roomInfo.roomName}
+                  </Text>
+                </View>
 
-              <View className="bg-box h-[14px] w-[1px] rounded-md mx-2" />
+                <View className="bg-box h-[14px] w-[1px] rounded-md mx-2" />
 
-              <View className="flex flex-row items-center">
-                <PersonIcon />
-                <Text className="ml-1 text-xs font-medium text-basicFont">
-                  {roomInfo.numOfMate}명
-                </Text>
+                <View className="flex flex-row items-center">
+                  <PersonIcon />
+                  <Text className="ml-1 text-xs font-medium text-basicFont">
+                    {roomInfo.numOfMate}명
+                  </Text>
+                </View>
               </View>
             </View>
-          </View>
 
-          <View className="flex flex-col items-end">
-            <ResetIcon />
-            <View className="mt-3">
-              {memberList.map((member) => (
-                <RoomMateBox
-                  name={member.name}
-                  profileImage={member.profileImage}
-                  state={member.state}
-                  isAdmin={member.isAdmin}
-                />
-              ))}
+            <View className="flex flex-col items-end mb-14">
+              <ResetIcon />
+              <View className="flex-row flex-wrap justify-between mt-8 flxex">
+                {fullMemberList.map((member, index) => (
+                  <RoomMateBox
+                    key={index}
+                    name={member.name}
+                    profileImage={member.profileImage}
+                    state={member.state}
+                    isAdmin={member.isAdmin}
+                  />
+                ))}
+              </View>
+            </View>
+
+            <View className="flex p-4 bg-main1 rounded-xl">
+              <Pressable>
+                <Text className="text-base font-semibold text-center text-white">방 삭제하기</Text>
+              </Pressable>
             </View>
           </View>
         </View>
-      </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
