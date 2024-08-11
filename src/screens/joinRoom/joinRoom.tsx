84bf67247cd62dd7f3f1a160d65489962c862d1d
JJHyeok13 <jjinh99@naver.com>
2024-08-12 03:27:53 +0900

    feat: 방 참여하기에서 돌아가기 네비게이션 추가

--- src/screens/joinRoom/joinRoom.tsx
+++ src/screens/joinRoom/joinRoom.tsx
@@ -1,9 +1,17 @@
+import { JoinRoomScreenProps } from '@type/param/loginStack';
 import React from 'react';
-import { Text, TextInput, View } from 'react-native';
+import { Pressable, Text, TextInput, View } from 'react-native';
 
-const JoinRoomScreen = () => {
+const JoinRoomScreen = ({ navigation }: JoinRoomScreenProps) => {
+  const toMain = () => {
+    navigation.navigate('MainScreen');
+  };
+
   return (
     <View>
+      <Pressable onPress={toMain}>
+        <Text>돌아가기</Text>
+      </Pressable>
       <Text>방장이 준 초대코드를 입력해주세요!</Text>
       <TextInput placeholder="초대코드를 입력해주세요" />
     </View>
