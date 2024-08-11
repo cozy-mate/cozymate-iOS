0dc773857eff5995ef2f94c497119acbb004b8f7
JJHyeok13 <jjinh99@naver.com>
2024-08-12 03:49:19 +0900

    feat: 룸메이트 대기 스크린 컴포넌트 생성

---
+++ src/screens/waitingRoom/waitingRoom.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { SafeAreaView } from 'react-native';
+
+const WaitingRoomScreen = () => {
+  return <SafeAreaView></SafeAreaView>;
+};
+
+export default WaitingRoomScreen;
