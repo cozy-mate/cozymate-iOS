e6c2e1a4fe7bfd78b0c7dc949bd1a5aedb515c4e
JJHyeok13 <jjinh99@naver.com>
2024-08-11 16:18:44 +0900

    feat: 마이페이지 스크린 컴포넌트 생성

---
+++ src/screens/myPage/myPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { SafeAreaView, Text, View } from 'react-native';
+
+const MyPageScreen = () => {
+  return (
+    <SafeAreaView>
+      <Text>마이페이지입니다.</Text>
+    </SafeAreaView>
+  );
+};
+
+export default MyPageScreen;
