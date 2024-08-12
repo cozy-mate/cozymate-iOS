69a117c8027caf3e8d8e411fdffa5eee3eea6b3a
JJHyeok13 <jjinh99@naver.com>
2024-08-13 01:00:29 +0900

    feat: axios 설정 및 dependency 설치

---
+++ src/axios/axios.Instance.ts
@@ -0,0 +1,54 @@
+import { deleteToken, getAccessToken } from '@utils/token';
+import axios, { AxiosInstance } from 'axios';
+
+const axiosInstance: AxiosInstance = axios.create({
+  baseURL: 'https://cozymate.store',
+  withCredentials: true,
+});
+
+axiosInstance.interceptors.request.use(
+  async (config) => {
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+
+//JWT 토큰 기반 API 요청 => 주석 처리
+axiosInstance.interceptors.request.use(
+  async (config) => {
+    const accessToken = getAccessToken();
+
+    if (!accessToken) {
+      // 토큰이 없을 경우 로그아웃 처리
+      throw new Error('토큰 없음');
+    }
+
+    config.headers['Authorization'] = `Bearer ${accessToken}`;
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+
+// 토큰 관련 에러 처리
+axiosInstance.interceptors.response.use(
+  async (response) => {
+    return response;
+  },
+
+  async (error) => {
+    // 토큰 만료나 잘못된 토큰일 때 로그아웃 처리
+    if (error.response?.data?.code === 'AUTH_001') {
+      console.log('잘못된 토큰');
+      deleteToken();
+    }
+
+    return Promise.reject(error);
+  },
+);
+
+export default axiosInstance;
---
+++ src/axios/axios.method.ts
@@ -0,0 +1,38 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { AxiosResponse, AxiosRequestConfig } from 'axios';
+
+import axiosInstance from '@axios/axios.Instance';
+
+export const PostAxiosInstance = async <T>(
+  url: string,
+  data?: any,
+  config?: AxiosRequestConfig,
+): Promise<AxiosResponse> => {
+  const response = await axiosInstance.post(url, data, config);
+  return response;
+};
+
+export const GetAxiosInstance = async <T>(
+  url: string,
+  config?: AxiosRequestConfig,
+): Promise<AxiosResponse> => {
+  const response = await axiosInstance.get(url, config);
+  return response;
+};
+
+export const PatchAxiosInstance = async <T>(
+  url: string,
+  data?: any,
+  config?: AxiosRequestConfig,
+): Promise<AxiosResponse> => {
+  const response = await axiosInstance.patch(url, data, config);
+  return response;
+};
+
+export const DeleteAxiosInstance = async <T>(
+  url: string,
+  config?: AxiosRequestConfig,
+): Promise<AxiosResponse> => {
+  const response = await axiosInstance.delete(url, config);
+  return response;
+};
