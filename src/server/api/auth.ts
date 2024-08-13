1ff62fc33b6fa5cf8220d9c91d05676c6c3466fa
JJHyeok13 <jjinh99@naver.com>
2024-08-14 04:03:54 +0900

    feat: 토큰 재발행 서버 도메인 변경

--- src/server/api/auth.ts
+++ src/server/api/auth.ts
@@ -3,7 +3,7 @@ import { ReissueTokenResponse } from '@server/responseTypes/auth';
 
 // 토큰 재발행
 export const reissueToken = async (refreshToken: string): Promise<ReissueTokenResponse> => {
-  const response = await GetAxiosInstance<ReissueTokenResponse>(`/member/reissue`, {
+  const response = await GetAxiosInstance<ReissueTokenResponse>(`/auth/reissue`, {
     headers: {
       Authorization: `Bearer ${refreshToken}`,
     },
