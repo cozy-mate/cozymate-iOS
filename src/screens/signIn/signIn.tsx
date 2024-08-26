78e56b90a4a23829426ace6a64b0ddd714c721f9
jpark0506 <peterp1@inha.edu>
2024-08-26 19:42:17 +0900

    [COZY-213] feat: 수동 로그인 구현 완료

--- src/screens/signIn/signIn.tsx
+++ src/screens/signIn/signIn.tsx
@@ -1,20 +1,16 @@
 import React from 'react';
-import { Pressable, Text, View, SafeAreaView } from 'react-native';
+import { Pressable, Text, View, SafeAreaView, Alert } from 'react-native';
 
 import KakaoLogo from '@assets/signIn/kakaoLogo.svg';
 import AppleLogo from '@assets/signIn/appleLogo.svg';
 
 import { SignInScreenProps } from '@type/param/rootStack';
 
-import { useKakaoLogin } from '@hooks/api/member';
+import { useKakaoLogin, useLoginWithId } from '@hooks/api/member';
 
 const SignInScreen = ({ navigation }: SignInScreenProps) => {
   const { mutateAsync: kakaoLogin, isPending: kakaoLoginPending } = useKakaoLogin(navigation);
-
-  const toOnBoard = () => {
-    navigation.navigate('PersonalInfoInputScreen');
-  };
-
+  const loginWithId = useLoginWithId(navigation);
   return (
     <SafeAreaView className="flex-1 bg-white">
       <View className="flex-1 mx-6">
@@ -45,7 +41,7 @@ const SignInScreen = ({ navigation }: SignInScreenProps) => {
         <View className="mx-3 mb-4">
           <Pressable
             className="flex-row items-center justify-center rounded-[33px] bg-appleblack px-6 py-4"
-            onPress={toOnBoard}
+            onPress={() => loginWithId()}
           >
             <AppleLogo className="mr-4" />
             <Text className="text-base font-semibold text-center text-white">Apple로 계속하기</Text>
