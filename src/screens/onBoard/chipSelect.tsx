ceeb3662132b04fbef2550a87c9050efb5cc82d5
JJHyeok13 <jjinh99@naver.com>
2024-10-16 02:44:57 +0900

    feat: 온보딩 칩 선택 스크린 생성

---
+++ src/screens/onBoard/chipSelect.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { SafeAreaView } from 'react-native';
+
+const ChipSelectScreen = () => {
+  return <SafeAreaView></SafeAreaView>;
+};
+
+export default ChipSelectScreen;
