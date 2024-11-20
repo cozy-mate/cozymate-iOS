537858104c1b6b67913f5c31d3106dd12532e62e
JJHyeok13 <jjinh99@naver.com>
2024-11-21 02:27:24 +0900

    feat: 학교 인증 코드 수정

--- src/screens/schoolAuthentication/schoolAuthentication.tsx
+++ src/screens/schoolAuthentication/schoolAuthentication.tsx
@@ -8,12 +8,13 @@ import {
   TouchableWithoutFeedback,
 } from 'react-native';
 
-import ButtonModal from '@components/common/buttonModal';
+import LoadingComponent from '@components/loading/loading';
 import UnivInfoSelect from '@components/onBoard/univInfoSelect';
+import OneButtonModal from '@components/commonComponents/oneButtonModal';
 import ButtonTextInput from '@components/schoolAuthentication/buttonTextInput';
 
-import { useGetUserUniversity } from '@hooks/api/university';
 import { useSendMail, useVerifyMail } from '@hooks/api/mail';
+import { useGetUniversityInfo } from '@hooks/api/university';
 
 import { SchoolAuthenticationScreenProps } from '@type/param/stack';
 
@@ -34,24 +35,35 @@ const SchoolAuthenticationScreen = ({ navigation, route }: SchoolAuthenticationS
   };
 
   const toBack = () => {
+    setIsModalOpen(false);
     navigation.goBack();
   };
 
   // 사용자 학교 정보 조회
-  const { data: userSchoolInfo } = useGetUserUniversity();
+  const { data: userSchoolInfo } = useGetUniversityInfo(1);
 
   // 인증메일 전송
-  const { mutateAsync: sendAuthenticationMail } = useSendMail(
-    mailAddress,
-    userSchoolInfo.result.id,
-  );
+  const { mutateAsync: sendAuthenticationMail, isPending: sendMailPending } = useSendMail();
 
   // 인증번호 확인
-  const { mutateAsync: verifyAuthenticationCode } = useVerifyMail(
-    authenticationCode,
-    userSchoolInfo.result.id,
-    majorName,
-  );
+  const { mutateAsync: verifyAuthenticationCode, isPending: verifyMailPending } = useVerifyMail();
+
+  const handleMailSend = async (): Promise<void> => {
+    await sendAuthenticationMail({
+      mailAddress: mailAddress,
+      universityId: userSchoolInfo.result.id,
+    });
+    setIsSended(true);
+  };
+
+  const handleVerifyCode = async (): Promise<void> => {
+    await verifyAuthenticationCode({
+      code: authenticationCode,
+      universityId: userSchoolInfo.result.id,
+      majorName: majorName,
+    });
+    setIsModalOpen(true);
+  };
 
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
@@ -129,10 +141,7 @@ const SchoolAuthenticationScreen = ({ navigation, route }: SchoolAuthenticationS
               setValue={setMailAddress}
               placeholder="이메일을 입력해주세요"
               buttonString={isSended ? '인증번호 재전송' : '인증번호 전송'}
-              buttonFunc={() => {
-                sendAuthenticationMail;
-                setIsSended(true);
-              }}
+              buttonFunc={handleMailSend}
               pattern={userSchoolInfo.result.mailPattern}
             />
 
@@ -143,23 +152,22 @@ const SchoolAuthenticationScreen = ({ navigation, route }: SchoolAuthenticationS
                 setValue={setAuthenticationCode}
                 placeholder="인증번호를 입력해주세요"
                 buttonString="인증번호 확인"
-                buttonFunc={() => {
-                  verifyAuthenticationCode;
-                  setIsModalOpen(true);
-                }}
+                buttonFunc={handleVerifyCode}
               />
             )}
 
-            <ButtonModal
-              title="학교인증이 완료됐어요"
-              submitText="확인"
+            <OneButtonModal
               isVisible={isModalOpen}
-              buttonCount={1}
-              closeModal={() => setIsModalOpen(false)}
-              onSubmit={toBack}
+              title="학교인증이 완료됐어요"
+              closeFunc={toBack}
+              buttonText="확인"
+              buttonFunc={toBack}
             />
           </View>
         )}
+
+        {sendMailPending && <LoadingComponent />}
+        {verifyMailPending && <LoadingComponent />}
       </SafeAreaView>
     </TouchableWithoutFeedback>
   );
