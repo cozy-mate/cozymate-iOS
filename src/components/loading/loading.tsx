a7da792a3d73a2853fe0adc79e1acec530b9bd77
JJHyeok13 <jjinh99@naver.com>
2024-11-21 02:29:42 +0900

    feat: 로딩 컴포넌트 코드 수정

--- src/components/loading/loading.tsx
+++ src/components/loading/loading.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
-import { View } from 'react-native';
-import { Image, Modal } from 'react-native';
+import { View, Image, Modal } from 'react-native';
 
 const LoadingComponent: React.FC = () => {
   return (
     <Modal transparent={true}>
       <View className="flex h-full w-full items-center justify-center bg-modalBack">
         <Image source={require('../../assets/loading.gif')} className="h-20 w-20" />
-        {/* <ActivityIndicator size="large" color="#68A4FF" /> */}
       </View>
     </Modal>
   );
