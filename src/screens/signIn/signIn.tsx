ecb6d01f82f919cfec276873f19afe5888fc8ed6
JJHyeok13 <jjinh99@naver.com>
2024-11-27 04:44:18 +0900

    feat: 테스트용 로그인 다시 추가

--- src/screens/signIn/signIn.tsx
+++ src/screens/signIn/signIn.tsx
@@ -11,8 +11,23 @@ import {
   NativeSyntheticEvent,
 } from 'react-native';
 
+import { useHasRoomStore, useRoomInfoStore } from '@zustand/room/room';
+import { useProfileStore, useLoggedInStore } from '@zustand/member/member';
+import {
+  useLifeStyleStore,
+  usePreferencesStore,
+  useHasLifeStyleStore,
+} from '@zustand/member-stat/member-stat';
+
+import { signIn, getMyProfile } from '@server/api/member';
+import { getMemberStatData } from '@server/api/member-stat';
+import { getRoomData, checkHasRoom } from '@server/api/room';
+import { getPreferenceList } from '@server/api/member-stat-preference';
+
 import useFcm from '@hooks/useFcm';
 import { useAppleLogin, useKakaoLogin } from '@hooks/api/member';
+
+import { setAccessToken, setRefreshToken } from '@utils/token';
 
 import { SignInScreenProps } from '@type/param/rootStack';
 
@@ -29,6 +44,67 @@ const SignInScreen = ({ navigation }: SignInScreenProps) => {
     getDeviceId();
     resetFcmToken();
   }, [getDeviceId, resetFcmToken]);
+
+  // 로그인 정보
+  const { setLoggedIn } = useLoggedInStore();
+  // 프로필 정보
+  const { setProfile } = useProfileStore();
+  const { setPreferenceList } = usePreferencesStore();
+  // 방 여부 및 방 정보
+  const { setMyRoom } = useHasRoomStore();
+  const { setRoomInfo } = useRoomInfoStore();
+  // 라이프스타일 여부 및 라이프스타일 정보
+  const { setHasLifeStyle } = useHasLifeStyleStore();
+  const { setLifeStyle } = useLifeStyleStore();
+
+  const test = async () => {
+    const response = await signIn({
+      clientId: 'YTBkODFmYTAtNTFiNC00NTllLWE5MWItYjM5ZTNkMTI4NGNl',
+      socialType: 'KAKAO',
+    });
+
+    const { accessToken, refreshToken } = response.result.tokenResponseDTO;
+
+    // 로그인 시도 후 기존 회원이면 accessToken / 신규 회원이면 임시 accessToken
+    await setAccessToken(accessToken);
+    await setRefreshToken(refreshToken);
+
+    // 프로필 정보 저장
+    const getProfileResponse = await getMyProfile();
+    setProfile(getProfileResponse.result);
+
+    const preferenceResponse = await getPreferenceList();
+    setPreferenceList(preferenceResponse.result.preferenceList);
+
+    // 방 존재 여부 저장
+    const roomCheckResponse = await checkHasRoom();
+    const roomId = roomCheckResponse.result.roomId;
+
+    // 방이 존재하는 경우 방 정보 저장
+    if (roomId !== 0) {
+      setMyRoom({ hasRoom: true, roomId: roomId });
+
+      const roomInfoResponse = await getRoomData(roomId);
+      setRoomInfo(roomInfoResponse.result);
+    }
+
+    // getUserDetailData 호출 및 라이프스타일 정보 처리
+    try {
+      const userDetailResponse = await getMemberStatData();
+      setHasLifeStyle(true);
+      setLifeStyle(userDetailResponse.result);
+    } catch (error: any) {
+      const errorCode = error?.response?.data?.code;
+      if (errorCode === 'MEMBERSTAT402') {
+        setHasLifeStyle(false);
+      } else {
+        // 예상하지 못한 에러 처리
+        console.error(error);
+      }
+    }
+
+    setLoggedIn(true);
+  };
 
   const { mutateAsync: kakaoLogin } = useKakaoLogin(navigation);
   const { mutateAsync: appleLogin } = useAppleLogin(navigation);
@@ -122,6 +198,14 @@ const SignInScreen = ({ navigation }: SignInScreenProps) => {
           <Pressable
             className="flex-row items-center justify-center rounded-[33px] bg-appleblack px-6 py-4"
             onPress={() => appleLogin()}
+          >
+            <AppleLogo className="mr-4" />
+            <Text className="text-center text-base font-semibold text-white">Apple로 계속하기</Text>
+          </Pressable>
+
+          <Pressable
+            className="flex-row items-center justify-center rounded-[33px] bg-appleblack px-6 py-4"
+            onPress={test}
           >
             <AppleLogo className="mr-4" />
             <Text className="text-center text-base font-semibold text-white">Apple로 계속하기</Text>
