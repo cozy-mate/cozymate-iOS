fdef0d65cd64d6a7c9efb9f813d266546eb6e86e
jpark0506 <peterp1@inha.edu>
2025-04-16 17:58:30 +0900

    [COZY-585] feat : Axios Error를 보기 좋게 만드는 refiner 추가

---
+++ error/refiner.ts
@@ -0,0 +1,14 @@
+import { AxiosError } from "axios";
+
+export const errorRefiner = (error : any) => {
+    if (error instanceof AxiosError) {
+        return {
+        status: error.response?.status,
+        message: error.message,
+        code: error.code,
+        url: error.config?.url,
+        };
+    } else {
+        return error
+    }
+}
