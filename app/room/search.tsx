4af0330abd300050c229dae499cbb76ebdd3004a
JJHyeok13 <jjinh99@naver.com>
2025-04-29 12:32:08 +0900

    feat: 방 검색 스크린 구현

---
+++ app/room/search.tsx
@@ -0,0 +1,80 @@
+import { useRouter } from 'expo-router';
+import { useState } from 'react';
+import { FlatList, Pressable, Text, TextInput, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+import XButton from '@/assets/images/common/roundX.svg';
+import { useSearchRoom } from '@/hooks/room/room';
+import { useDebounce } from '@/hooks/useDebounce';
+
+export default function SearchRoom() {
+  const router = useRouter();
+
+  const [keyword, setKeyword] = useState<string>('');
+  const debouncedKeyword = useDebounce(keyword, 500);
+
+  const { data } = useSearchRoom(debouncedKeyword);
+
+  return (
+    <SafeAreaView className="flex-1 bg-white">
+      <View className="flex-1 px-[20px] mt-[16px]">
+        <View className="flex flex-row items-center gap-x-[8px] mb-[24px]">
+          <View className="flex-1 relative">
+            <TextInput
+              value={keyword}
+              onChangeText={(e: string) => setKeyword(e)}
+              placeholder="방 이름을 입력해주세요"
+              className="bg-colorBox rounded-xl p-[16px] text-14 font-500 text-basicFont pr-[40px]"
+            />
+            {keyword !== '' && (
+              <Pressable
+                onPress={() => setKeyword('')}
+                className="absolute top-[17px] right-[16px]"
+              >
+                <XButton />
+              </Pressable>
+            )}
+          </View>
+
+          <Pressable onPress={() => router.back()} className="px-[8px] py-[11.5px]">
+            <Text className="text-14 font-500 leading-14 text-emphasizedFont">취소</Text>
+          </Pressable>
+        </View>
+
+        {data !== undefined && (
+          <FlatList
+            data={data?.result}
+            renderItem={({ item }) => (
+              <Pressable
+                key={item.roomId}
+                onPress={() => router.push(`/room/${item.roomId}`)}
+                className="flex flex-row justify-between items-center px-[8px] py-[10px]"
+              >
+                <View>
+                  <Text className="text-16 font-600 leading-16 text-emphasizedFont">
+                    {item.name}
+                  </Text>
+                  <Text className="text-12 font-500 leading-12 text-disabledFont">
+                    <Text className="text-mainColor">{item.arrivalMateNum}명</Text>의 룸메이트가
+                    있어요
+                  </Text>
+                </View>
+
+                <Text className="text-16 font-500 leading-16 text-mainColor">{item.equality}%</Text>
+              </Pressable>
+            )}
+            ItemSeparatorComponent={() => <View className="h-[1px] bg-[#F6F6F6] my-[12px]" />}
+            contentContainerStyle={data.result.length === 0 ? { flexGrow: 1 } : undefined}
+            ListEmptyComponent={() => (
+              <View className="flex-1 justify-center items-center">
+                <Text className="text-14 font-500 leading-14 text-disabledFont text-center mb-[75px]">
+                  검색결과가 없어요
+                </Text>
+              </View>
+            )}
+          />
+        )}
+      </View>
+    </SafeAreaView>
+  );
+}
