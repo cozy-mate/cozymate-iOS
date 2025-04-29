b18cd6ede011692835557a810232345fcf3b2452
JJHyeok13 <jjinh99@naver.com>
2025-04-29 12:40:36 +0900

    chore: eslint, prettier 적용

--- error/refiner.ts
+++ error/refiner.ts
@@ -1,14 +1,14 @@
-import { AxiosError } from "axios";
+import { AxiosError } from 'axios';
 
-export const errorRefiner = (error : any) => {
-    if (error instanceof AxiosError) {
-        return {
-        status: error.response?.status,
-        message: error.message,
-        code: error.code,
-        url: error.config?.url,
-        };
-    } else {
-        return error
-    }
-}
+export const errorRefiner = (error: any) => {
+  if (error instanceof AxiosError) {
+    return {
+      status: error.response?.status,
+      message: error.message,
+      code: error.code,
+      url: error.config?.url,
+    };
+  } else {
+    return error;
+  }
+};
