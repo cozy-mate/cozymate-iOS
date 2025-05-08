864a7453910522337e419ad107350370bd4ebc0a
JJHyeok13 <jjinh99@naver.com>
2025-05-08 15:53:53 +0900

    feat: 초대코드로 방 참여 기능 추가

--- app/room/joinRoom.tsx
+++ app/room/joinRoom.tsx
@@ -5,9 +5,24 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import BackHeaderComponent from '@/components/common/backHeader';
 import BottomButton from '@/components/common/bottomButton';
 import CustomTextInputComponent from '@/components/common/customTextInput';
+import OneButtonModal from '@/components/common/oneButtonModal';
+import TwoButtonModal from '@/components/common/twoButtonModal';
+import { useGetRoomByInviteCode, useJoinRoom } from '@/hooks/room/room';
+import { RoomItem } from '@/type/room';
 
 export default function JoinRoom() {
   const [inviteCode, setInviteCode] = useState<string>('');
+
+  const [roomInfo, setRoomInfo] = useState<RoomItem | null>(null);
+  const [isRoomInfoModalOpen, setIsRoomInfoModalOpen] = useState<boolean>(false);
+  const [isWrongInviteCodeModalOpen, setIsWrongInviteCodeModalOpen] = useState<boolean>(false);
+
+  const { mutateAsync: getRoomByInviteCode } = useGetRoomByInviteCode(
+    setRoomInfo,
+    setIsRoomInfoModalOpen,
+    setIsWrongInviteCodeModalOpen,
+  );
+  const { mutateAsync: joinRoom } = useJoinRoom();
 
   return (
     <SafeAreaView className="flex-1 bg-white">
@@ -25,9 +40,37 @@ export default function JoinRoom() {
         <BottomButton
           buttonText="확인"
           disabled={inviteCode.length === 0}
-          onPress={() => console.log('클릭')}
+          onPress={() => getRoomByInviteCode(inviteCode)}
         />
       </View>
+
+      {roomInfo !== null && (
+        <TwoButtonModal
+          isVisible={isRoomInfoModalOpen}
+          title={`[${roomInfo.name}] 방이 맞나요?`}
+          subtitle={`방장 : [${roomInfo.managerNickname}] | ${roomInfo.maxMateNum}인실`}
+          closeFunc={() => setIsRoomInfoModalOpen(false)}
+          leftButtonText="취소"
+          leftButtonFunc={() => setIsRoomInfoModalOpen(false)}
+          rightButtonText="확인"
+          rightButtonFunc={() => joinRoom(roomInfo.roomId)}
+        />
+      )}
+
+      <OneButtonModal
+        isVisible={isWrongInviteCodeModalOpen}
+        title="초대코드를 다시 확인해주세요!"
+        subtitle="해당 코드를 가진 방이 존재하지 않아요"
+        closeFunc={() => {
+          setIsWrongInviteCodeModalOpen(false);
+          setInviteCode('');
+        }}
+        buttonText="확인"
+        buttonFunc={() => {
+          setIsWrongInviteCodeModalOpen(false);
+          setInviteCode('');
+        }}
+      />
     </SafeAreaView>
   );
 }
