f98eff25ccbcfa44f933a38d44498b66f80507f3
JJHyeok13 <jjinh99@naver.com>
2025-06-11 04:36:30 +0900

    chore: 컴포넌트 구조 수정

---
+++ components/userDetail/basicInfo.tsx
@@ -0,0 +1,50 @@
+import { Text, View } from 'react-native';
+
+import { UserDetailComponentProps } from '@/type/member-stat';
+
+const BasicInfoComponent: React.FC<UserDetailComponentProps> = ({ data }) => {
+  return (
+    <View className="px-[20px] gap-y-[12px]">
+      <Text className="Semibold16 text-emphasizedFont ml-[4px]">기본 정보</Text>
+
+      <View className="p-[16px] rounded-xl border border-strokeColor">
+        <View className="flex flex-row items-center gap-x-[12px]">
+          <Text className="Medium14 text-colorFont">닉네임</Text>
+          <Text className="Medium14 text-basicFont">{data.memberDetail.nickname}</Text>
+        </View>
+
+        <View className="bg-[#F1F2F4] h-[1px] my-[12px]" />
+
+        <View className="flex flex-row items-center gap-x-[12px]">
+          <Text className="Medium14 text-colorFont">출생년도</Text>
+          <Text className="Medium14 text-basicFont">
+            {data.memberDetail.birthday.slice(0, 4)}년
+          </Text>
+        </View>
+
+        <View className="bg-[#F1F2F4] h-[1px] my-[12px]" />
+
+        <View className="flex flex-row items-center gap-x-[12px]">
+          <Text className="Medium14 text-colorFont">학교</Text>
+          <Text className="Medium14 text-basicFont">{data.memberDetail.universityName}</Text>
+        </View>
+
+        <View className="bg-[#F1F2F4] h-[1px] my-[12px]" />
+
+        <View className="flex flex-row items-center gap-x-[12px]">
+          <Text className="Medium14 text-colorFont">학번</Text>
+          <Text className="Medium14 text-basicFont">{data.memberStatDetail.admissionYear}학번</Text>
+        </View>
+
+        <View className="bg-[#F1F2F4] h-[1px] my-[12px]" />
+
+        <View className="flex flex-row items-center gap-x-[12px]">
+          <Text className="Medium14 text-colorFont">학과</Text>
+          <Text className="Medium14 text-basicFont">{data.memberDetail.majorName}</Text>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default BasicInfoComponent;
