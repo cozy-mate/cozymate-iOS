b1aff8620dba624d8a4ecffc34a264aa5e5c7a7a
JJHyeok13 <jjinh99@naver.com>
2025-07-09 20:36:30 +0900

    fix: 화면 사이즈에 따른 온보딩 화면 수정

--- app/(onBoard)/index.tsx
+++ app/(onBoard)/index.tsx
@@ -16,6 +16,7 @@ export default function HomeScreen() {
   const progress = useSharedValue<number>(0);
 
   const width = Dimensions.get('screen').width;
+  const isSmallSize = Dimensions.get('screen').height <= 667;
 
   const LottieItems = [
     {
@@ -24,23 +25,23 @@ export default function HomeScreen() {
       subtitle: '정형화된 라이프스타일로\n나와 꼭 맞는 룸메이트를 쉽고 빠르게 찾아봐요',
       path: require('@/assets/lotties/findRoomMate.json'),
       width: width,
-      height: 315,
+      height: isSmallSize ? 285 : 315,
     },
     {
       index: 2,
       title: '학교인증으로 신뢰성 UP!',
       subtitle: '학교 이메일을 통한 학교 인증으로,\n신뢰성을 높였어요',
       path: require('@/assets/lotties/schoolAuthentication.json'),
-      width: 277,
-      height: 283,
+      width: isSmallSize ? 285 : 277,
+      height: isSmallSize ? 285 : 283,
     },
     {
       index: 3,
       title: '롤앤룰로 공동체 생활을 더 윤택하게!',
       subtitle: '우리방의 역할과 규칙을 정하고,\n서로 역할을 잘 수행하고 있는지 확인할 수 있어요',
       path: require('@/assets/lotties/roleNrule.json'),
-      width: 277,
-      height: 283,
+      width: isSmallSize ? 285 : 277,
+      height: isSmallSize ? 285 : 283,
     },
   ];
 
@@ -50,7 +51,7 @@ export default function HomeScreen() {
 
       <Carousel
         width={width}
-        height={500}
+        height={isSmallSize ? 450 : 500}
         loop={true}
         data={LottieItems}
         snapEnabled={true}
@@ -59,13 +60,16 @@ export default function HomeScreen() {
         autoPlayInterval={5000}
         onProgressChange={progress}
         renderItem={({ item }) => (
-          <View className="mt-[50px] flex flex-col justify-between items-center h-[400px]">
+          <View
+            className={`flex flex-col justify-between items-center h-[400px] ${isSmallSize ? 'mt-[24px]' : 'mt-[50px]'} `}
+          >
             <LottieView
               source={item.path}
               style={{
                 width: item.width,
                 height: item.height,
               }}
+              resizeMode={item.index === 1 ? 'cover' : 'contain'}
               autoPlay
               loop
             />
@@ -91,7 +95,9 @@ export default function HomeScreen() {
         containerStyle={{ gap: 8 }}
       />
 
-      <View className="absolute bottom-[91px] gap-y-[12px] w-full px-[38px]">
+      <View
+        className={`absolute ${isSmallSize ? 'bottom-[32px]' : 'bottom-[91px]'}  gap-y-[12px] w-full px-[38px]`}
+      >
         <TouchableOpacity
           className="flex-row gap-x-[8px] items-center justify-center rounded-[33px] bg-kakaoyellow px-[24px] py-[16px]"
           onPress={() => kakaoLogin()}
