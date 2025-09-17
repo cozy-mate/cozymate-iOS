d10938eff42eedad4263b7e3df6c2a37e1af579e
JJHyeok13 <jjinh99@naver.com>
2025-09-17 18:41:54 +0900

    feat: 선택 아이템 바텀시트 추가

--- components/roleNRule/rule/ruleContainer.tsx
+++ components/roleNRule/rule/ruleContainer.tsx
@@ -1,11 +1,15 @@
 import BottomSheet from '@gorhom/bottom-sheet';
+import { Fragment, RefObject, useRef } from 'react';
 import { Pressable, Text, View } from 'react-native';
 
 import SettingIcon from '@/assets/icons/setting.svg';
-import { useCheckHasRoom, useGetMyRoomDetail } from '@/hooks/room/room';
+import { useGetMyRoomDetail } from '@/hooks/room/room';
 import { useSelectedItemStore } from '@/zustand/roleNRule/roleNRule';
+import { useMemberStore } from '@/zustand/store';
+
+import RoleNRuleBottomSheet from '../bottomSheet';
+
 import RuleBox from './ruleSkeleton';
-import { RefObject } from 'react';
 
 type RuleItem = {
   ruleId: number;
@@ -16,69 +20,76 @@ type RuleItem = {
 interface RuleContainerProps {
   isFetching: boolean;
   data: RuleItem[] | undefined;
-  bottomSheetRef: RefObject<BottomSheet>;
 }
 
-export default function RuleContainer({ isFetching, data, bottomSheetRef }: RuleContainerProps) {
-  const { data: hasRoom } = useCheckHasRoom();
+export default function RuleContainer({ isFetching, data }: RuleContainerProps) {
+  const { roomInfo } = useMemberStore();
 
-  const { data: roomData } = useGetMyRoomDetail(hasRoom.result.roomId);
+  const { data: roomData } = useGetMyRoomDetail(roomInfo?.roomId ?? 0);
 
   const { setSelectedItem } = useSelectedItemStore();
 
+  const bottomSheetRef = useRef<BottomSheet>(null);
+
   return (
-    <View className="gap-y-[12px]">
-      <Text className="Semibold18 text-basicFont">
-        <Text className="text-mainColor">{roomData?.result.name}</Text>의{'\n'}규칙에 대해
-        알려드릴게요!
-      </Text>
+    <Fragment>
+      <View className="gap-y-[12px]">
+        <Text className="Semibold18 text-basicFont">
+          <Text className="text-mainColor">{roomData?.result.name}</Text>의{'\n'}규칙에 대해
+          알려드릴게요!
+        </Text>
 
-      {isFetching ? (
-        <RuleBox />
-      ) : data !== undefined && data.length !== 0 ? (
-        <View className="p-[8px] pl-[16px] rounded-xl bg-white shadow-chipback">
-          {data.map((rule, index) => (
-            <View
-              key={rule.ruleId}
-              className="flex flex-row items-start justify-between gap-x-[6px]"
-            >
-              <View className="flex flex-row items-center gap-x-[8px] mt-[8px]">
-                <View className="w-[24px] h-[24px] rounded-full bg-colorBox flex items-center justify-center">
-                  <Text className="Medium12 text-colorFont text-center">{index + 1}</Text>
-                </View>
-              </View>
-
-              <View className="flex-1 mt-[8px]">
-                <Text className="Medium14 text-basicFont my-[3.5px]">{rule.content}</Text>
-
-                {rule.memo !== '' && (
-                  <Text className="Medium10 text-disabledFont mt-[2px]">{rule.memo}</Text>
-                )}
-              </View>
-
-              <Pressable
-                onPress={() => {
-                  setSelectedItem((prev) => ({
-                    ...prev,
-                    id: rule.ruleId,
-                    type: 'Rule',
-                    content: rule.content,
-                    ruleItem: rule,
-                  }));
-                  bottomSheetRef.current?.expand();
-                }}
-                className="p-[8px]"
+        {isFetching ? (
+          <RuleBox />
+        ) : data !== undefined && data.length !== 0 ? (
+          <View className="p-[8px] pl-[16px] rounded-xl bg-white shadow-chipback">
+            {data.map((rule, index) => (
+              <View
+                key={rule.ruleId}
+                className="flex flex-row items-start justify-between gap-x-[6px]"
               >
-                <SettingIcon />
-              </Pressable>
-            </View>
-          ))}
-        </View>
-      ) : (
-        <View className="bg-white h-[144px] flex items-center justify-center rounded-xl shadow-chipback">
-          <Text className="Medium14 text-disabledFont text-center">등록된 규칙이 없어요!</Text>
-        </View>
-      )}
-    </View>
+                <View className="flex flex-row items-center gap-x-[8px] mt-[8px]">
+                  <View className="w-[24px] h-[24px] rounded-full bg-colorBox flex items-center justify-center">
+                    <Text className="Medium12 text-colorFont text-center">{index + 1}</Text>
+                  </View>
+                </View>
+
+                <View className="flex-1 mt-[8px]">
+                  <Text className="Medium14 text-basicFont my-[3.5px]">{rule.content}</Text>
+
+                  {rule.memo !== '' && (
+                    <Text className="Medium10 text-disabledFont mt-[2px]">{rule.memo}</Text>
+                  )}
+                </View>
+
+                <Pressable
+                  onPress={() => {
+                    setSelectedItem((prev) => ({
+                      ...prev,
+                      id: rule.ruleId,
+                      content: rule.content,
+                      memo: rule.memo,
+                    }));
+                    bottomSheetRef.current?.expand();
+                  }}
+                  className="p-[8px]"
+                >
+                  <SettingIcon />
+                </Pressable>
+              </View>
+            ))}
+          </View>
+        ) : (
+          <View className="bg-white h-[144px] flex items-center justify-center rounded-xl shadow-chipback">
+            <Text className="Medium14 text-disabledFont text-center">등록된 규칙이 없어요!</Text>
+          </View>
+        )}
+      </View>
+
+      <RoleNRuleBottomSheet
+        type="rules"
+        bottomSheetRef={bottomSheetRef as RefObject<BottomSheet>}
+      />
+    </Fragment>
   );
 }
--- components/roleNRule/todo/myTodo.tsx
+++ components/roleNRule/todo/myTodo.tsx
@@ -1,4 +1,5 @@
 import BottomSheet from '@gorhom/bottom-sheet';
+import { Fragment, RefObject, useRef } from 'react';
 import { Pressable, Text, View } from 'react-native';
 
 import DoneIcon from '@/assets/images/roleNRule/done.svg';
@@ -9,9 +10,10 @@ import { MemberTodo } from '@/type/todo';
 import { formatDateToKorean } from '@/utils/translateDate';
 import { useSelectedItemStore } from '@/zustand/roleNRule/roleNRule';
 import { useMemberStore } from '@/zustand/store';
+
+import RoleNRuleBottomSheet from '../bottomSheet';
+
 import MyTodoSkeleton from './myTodoSkeleton';
-import { useRef } from 'react';
-import { useRouter } from 'expo-router';
 
 interface MyTodoComponentProps {
   isFetching: boolean;
@@ -20,11 +22,9 @@ interface MyTodoComponentProps {
 }
 
 export default function MyTodoComponent({ isFetching, timePoint, data }: MyTodoComponentProps) {
-  const router = useRouter();
-
   const { memberInfo, roomInfo } = useMemberStore();
 
-  const { mutateAsync: toggleTodo } = useToggleTodoDone(roomInfo?.roomId ?? 0, timePoint);
+  const { mutate: toggleTodo } = useToggleTodoDone(roomInfo?.roomId ?? 0, timePoint);
 
   const { setSelectedItem } = useSelectedItemStore();
 
@@ -44,58 +44,65 @@ export default function MyTodoComponent({ isFetching, timePoint, data }: MyTodoC
   };
 
   return (
-    <View className="gap-y-[12px]">
-      <Text className="Semibold18 text-basicFont mx-[4px]">
-        <Text className="text-mainColor">{formatDateToKorean(timePoint)}, </Text>
-        {` ${memberInfo?.nickname ?? ''}`}님이{'\n'}해야할 일들을 알려드릴게요!
-      </Text>
+    <Fragment>
+      <View className="gap-y-[12px]">
+        <Text className="Semibold18 text-basicFont mx-[4px]">
+          <Text className="text-mainColor">{formatDateToKorean(timePoint)}, </Text>
+          {`${memberInfo?.nickname ?? ''}`}님이{'\n'}해야할 일들을 알려드릴게요!
+        </Text>
 
-      {isFetching ? (
-        <MyTodoSkeleton />
-      ) : data !== undefined && data.todoList.length !== 0 ? (
-        <View className="p-[8px] rounded-xl bg-white shadow-chipback">
-          {data.todoList.map((todo) => (
-            <View key={todo.todoId} className="flex flex-row items-center justify-between">
-              <View className="flex flex-row items-center">
-                <Pressable
-                  onPress={() => toggleTodo({ todoId: todo.todoId, completed: !todo.completed })}
-                  className="p-[9px]"
-                >
-                  {todo.completed ? <DoneIcon /> : <NotDoneIcon />}
-                </Pressable>
-                <View className="flex flex-row items-center gap-x-[6px]">
-                  <Text className="Medium14 text-basicFont">{todo.content}</Text>
-                  <View
-                    className={`w-[6px] h-[6px] rounded-full ${handleTodoType(todo.todoType)}`}
-                  />
+        {isFetching && !data ? (
+          <MyTodoSkeleton />
+        ) : data !== undefined && data.todoList.length !== 0 ? (
+          <View className="p-[8px] rounded-xl bg-white shadow-chipback">
+            {data.todoList.map((todo) => (
+              <View key={todo.todoId} className="flex flex-row items-center justify-between">
+                <View className="flex flex-row items-center">
+                  <Pressable
+                    onPress={() => toggleTodo({ todoId: todo.todoId, completed: !todo.completed })}
+                    className="p-[9px]"
+                  >
+                    {todo.completed ? <DoneIcon /> : <NotDoneIcon />}
+                  </Pressable>
+                  <View className="flex flex-row items-center gap-x-[6px]">
+                    <Text className="Medium14 text-basicFont">{todo.content}</Text>
+                    <View
+                      className={`w-[6px] h-[6px] rounded-full ${handleTodoType(todo.todoType)}`}
+                    />
+                  </View>
                 </View>
-              </View>
 
-              <Pressable
-                onPress={() => {
-                  setSelectedItem((prev) => ({
-                    ...prev,
-                    id: todo.todoId,
-                    type: 'To-do',
-                    content: todo.content,
-                    todoItem: { ...todo, timePoint },
-                  }));
-                  bottomSheetRef.current?.expand();
-                }}
-                className="p-[8px]"
-              >
-                <SettingIcon />
-              </Pressable>
-            </View>
-          ))}
-        </View>
-      ) : (
-        <View className="bg-white h-[136px] flex items-center justify-center rounded-xl shadow-chipback">
-          <Text className="Medium14 text-disabledFont text-center">
-            오늘 등록된 할 일이 없어요!
-          </Text>
-        </View>
-      )}
-    </View>
+                <Pressable
+                  onPress={() => {
+                    setSelectedItem((prev) => ({
+                      ...prev,
+                      id: todo.todoId,
+                      mateIdList: todo.mateIdList,
+                      content: todo.content,
+                      timePoint: timePoint,
+                    }));
+                    bottomSheetRef.current?.expand();
+                  }}
+                  className="p-[8px]"
+                >
+                  <SettingIcon />
+                </Pressable>
+              </View>
+            ))}
+          </View>
+        ) : (
+          <View className="bg-white h-[136px] flex items-center justify-center rounded-xl shadow-chipback">
+            <Text className="Medium14 text-disabledFont text-center">
+              오늘 등록된 할 일이 없어요!
+            </Text>
+          </View>
+        )}
+      </View>
+
+      <RoleNRuleBottomSheet
+        type="todos"
+        bottomSheetRef={bottomSheetRef as RefObject<BottomSheet>}
+      />
+    </Fragment>
   );
 }
