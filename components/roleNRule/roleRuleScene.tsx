6d62ce4dc7b574c2053da135a6ddce850456de26
JJHyeok13 <jjinh99@naver.com>
2025-09-17 18:42:21 +0900

    feat: 조회 스크린 refreshControl 적용

--- components/roleNRule/roleRuleScene.tsx
+++ components/roleNRule/roleRuleScene.tsx
@@ -1,19 +1,33 @@
-import { ScrollView } from 'react-native';
-import RuleContainer from './rule/ruleContainer';
-import RoleContainer from './role/roleContainer';
-import { useGetRuleList } from '@/hooks/rule/rule';
-import { useGetRoleList } from '@/hooks/role/role';
-import { useMemberStore } from '@/zustand/store';
 import BottomSheet from '@gorhom/bottom-sheet';
-import { RefObject, useRef } from 'react';
+import { RefObject, useCallback, useRef } from 'react';
+import { RefreshControl, ScrollView } from 'react-native';
+
+import { useGetRoleList } from '@/hooks/role/role';
+import { useGetRuleList } from '@/hooks/rule/rule';
+import { useMemberStore } from '@/zustand/store';
+
+import RoleContainer from './role/roleContainer';
+import RuleContainer from './rule/ruleContainer';
 
 export default function RoleNRuleScene() {
   const { roomInfo } = useMemberStore();
 
-  const { data: ruleData, isFetching: isRuleFetching } = useGetRuleList(roomInfo?.roomId ?? 0);
-  const { data: roleData, isFetching: isRoleFetching } = useGetRoleList(roomInfo?.roomId ?? 0);
+  const {
+    data: ruleData,
+    isLoading: isRuleLoading,
+    isFetching: isRuleFetching,
+    refetch: ruleRefetch,
+  } = useGetRuleList(roomInfo?.roomId ?? 0);
+  const {
+    data: roleData,
+    isLoading: isRoleLoading,
+    isFetching: isRoleFetching,
+    refetch: roleRefetch,
+  } = useGetRoleList(roomInfo?.roomId ?? 0);
 
-  const bottomSheetRef = useRef<BottomSheet>(null);
+  const onRefresh = useCallback(async () => {
+    await Promise.all([ruleRefetch(), roleRefetch()]);
+  }, [ruleRefetch, roleRefetch]);
 
   return (
     <ScrollView
@@ -25,18 +39,18 @@ export default function RoleNRuleScene() {
         rowGap: 56,
         backgroundColor: '#F7FAFF',
       }}
+      refreshControl={
+        <RefreshControl
+          // 초기 상태에서는 나타나지 않도록 적용
+          refreshing={!isRuleLoading && !isRoleLoading && (isRuleFetching || isRoleFetching)}
+          onRefresh={onRefresh}
+          tintColor={'#68A4FF'}
+        />
+      }
     >
-      <RuleContainer
-        isFetching={isRuleFetching}
-        data={ruleData?.result}
-        bottomSheetRef={bottomSheetRef as RefObject<BottomSheet>}
-      />
+      <RuleContainer isFetching={isRuleFetching} data={ruleData?.result} />
 
-      <RoleContainer
-        isFetching={isRoleFetching}
-        data={roleData?.result}
-        bottomSheetRef={bottomSheetRef as RefObject<BottomSheet>}
-      />
+      <RoleContainer isFetching={isRoleFetching} data={roleData?.result} />
     </ScrollView>
   );
 }
--- components/roleNRule/todoScene.tsx
+++ components/roleNRule/todoScene.tsx
@@ -1,18 +1,31 @@
-import { ScrollView } from 'react-native';
-import CustomCalendar from './todo/calendar';
-import { useState } from 'react';
 import moment from 'moment';
+import { useCallback, useState } from 'react';
+import { RefreshControl, ScrollView } from 'react-native';
+
 import { useGetTodoList } from '@/hooks/todo/todo';
 import { useMemberStore } from '@/zustand/store';
-import MyTodoComponent from './todo/myTodo';
+
+import CustomCalendar from './todo/calendar';
 import MateTodoComponent from './todo/mateTodo';
+import MyTodoComponent from './todo/myTodo';
 
 export default function TodoScene() {
   const { roomInfo } = useMemberStore();
 
   const [timePoint, setTimePoint] = useState<string>(moment().format('YYYY-MM-DD'));
 
-  const { data, isFetching } = useGetTodoList(Number(roomInfo?.roomId ?? 0), timePoint);
+  const { data, isFetching, refetch } = useGetTodoList(Number(roomInfo?.roomId ?? 0), timePoint);
+
+  // TimePoint가 변경되었을 경우에는 RefreshControl이 보이지 않도록 커스텀 상태 활용
+  const [isRefreshing, setIsRefreshing] = useState(false);
+  const onRefresh = useCallback(async () => {
+    setIsRefreshing(true);
+    try {
+      await refetch();
+    } finally {
+      setIsRefreshing(false);
+    }
+  }, [refetch]);
 
   return (
     <ScrollView
@@ -24,6 +37,9 @@ export default function TodoScene() {
         rowGap: 32,
         backgroundColor: '#F7FAFF',
       }}
+      refreshControl={
+        <RefreshControl refreshing={isRefreshing} onRefresh={onRefresh} tintColor={'#68A4FF'} />
+      }
     >
       <CustomCalendar
         canSelectPrev={true}
