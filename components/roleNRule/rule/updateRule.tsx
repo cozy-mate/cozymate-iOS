1258688130e4b8294fa018020abda08021b99eec
JJHyeok13 <jjinh99@naver.com>
2025-09-17 18:41:10 +0900

    refactor: 롤앤룰 수정 스크린 리팩토링

---
+++ components/roleNRule/rule/updateRule.tsx
@@ -0,0 +1,73 @@
+import { useState } from 'react';
+import { Pressable, Text, TextInput, View } from 'react-native';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+
+import { useUpdateRule } from '@/hooks/rule/rule';
+import { useSelectedItemStore } from '@/zustand/roleNRule/roleNRule';
+import { useMemberStore } from '@/zustand/store';
+
+export default function UpdateRuleScene() {
+  const { roomInfo } = useMemberStore();
+
+  const { selectedItem } = useSelectedItemStore();
+
+  const { mutateAsync: updateRule } = useUpdateRule(roomInfo?.roomId ?? 0, selectedItem.id);
+
+  const [content, setContent] = useState(selectedItem.content);
+  const [memo, setMemo] = useState(selectedItem.memo);
+
+  return (
+    <View className="flex-1">
+      <KeyboardAwareScrollView
+        contentContainerStyle={{
+          flexGrow: 1,
+          paddingTop: 24,
+          paddingHorizontal: 20,
+          paddingBottom: 80,
+          rowGap: 48,
+        }}
+        keyboardShouldPersistTaps="handled"
+        enableOnAndroid={true}
+        extraScrollHeight={20}
+      >
+        <View className="gap-y-[12px]">
+          <Text className="Semibold16 text-emphasizedFont mx-[4px]">규칙을 입력해주세요</Text>
+          <TextInput
+            value={content}
+            onChangeText={(e) => setContent(e)}
+            placeholder="규칙을 입력해주세요"
+            placeholderTextColor={'#ACADB4'}
+            className={`px-[16px] py-[15px] rounded-xl InputMedium14 text-basicFont h-[49px] border`}
+          />
+        </View>
+
+        <View className="gap-y-[12px]">
+          <Text className="Semibold16 text-emphasizedFont mx-[4px]">
+            메모를 추가해주세요! <Text className="text-disabledFont">(선택)</Text>
+          </Text>
+          <TextInput
+            value={memo}
+            onChangeText={(e) => setMemo(e)}
+            placeholder="내용을 입력해주세요"
+            placeholderTextColor={'#ACADB4'}
+            className={`h-[120px] p-[16px] rounded-xl InputMedium14 text-basicFont border`}
+            autoCapitalize="none"
+            multiline={true}
+            // onFocus={onFocus}
+            // onBlur={onBlur}
+          />
+        </View>
+      </KeyboardAwareScrollView>
+
+      <Pressable
+        onPress={() => updateRule({ content, memo })}
+        disabled={!(content !== '')}
+        className={`${
+          content !== '' ? 'bg-mainColor' : 'bg-[#C4C4C4]'
+        }  py-[17.5px] mx-[20px] my-[8px] rounded-xl`}
+      >
+        <Text className="Semibold16 text-white text-center">확인</Text>
+      </Pressable>
+    </View>
+  );
+}
---
+++ components/roleNRule/todo/updateTodo.tsx
@@ -0,0 +1,136 @@
+import LottieView from 'lottie-react-native';
+import { useState } from 'react';
+import { Pressable, Text, TextInput, View } from 'react-native';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+
+import CheckBoxIcon from '@/assets/images/roleNRule/check.svg';
+import NotCheckBoxIcon from '@/assets/images/roleNRule/notCheck.svg';
+import { useGetMyRoomDetail } from '@/hooks/room/room';
+import { useUpdateTodo } from '@/hooks/todo/todo';
+import { useSelectedItemStore } from '@/zustand/roleNRule/roleNRule';
+import { useMemberStore } from '@/zustand/store';
+
+import CustomCalendar from './calendar';
+
+export default function UpdateTodoScene() {
+  const { roomInfo } = useMemberStore();
+
+  const { selectedItem } = useSelectedItemStore();
+
+  const { mutateAsync: updateTodo } = useUpdateTodo(roomInfo?.roomId ?? 0, selectedItem.id);
+
+  const [content, setContent] = useState<string>(selectedItem.content);
+  const [mateIdList, setMateIdList] = useState<number[]>(selectedItem.mateIdList);
+  const [timePoint, setTimePoint] = useState(selectedItem.timePoint);
+
+  const { data: memberList, isLoading } = useGetMyRoomDetail(roomInfo?.roomId ?? 0);
+
+  const toggleSelection = (itemValue: number) => {
+    const updatedValue = mateIdList.includes(itemValue)
+      ? mateIdList.filter((v) => v !== itemValue)
+      : [...mateIdList, itemValue];
+    setMateIdList(updatedValue);
+  };
+
+  const toggleAll = () => {
+    if (mateIdList.length === memberList?.result.mateDetailList.length) {
+      setMateIdList([]);
+    } else {
+      setMateIdList(memberList?.result.mateDetailList?.map((item) => item.mateId) ?? []);
+    }
+  };
+
+  const isReady = !isLoading && memberList !== undefined;
+
+  return (
+    <View className="flex-1">
+      {!isReady ? (
+        <View className="flex-grow justify-center items-center">
+          <LottieView
+            source={require('@/assets/lotties/cozymateLoading.json')}
+            style={{ width: 76, height: 94.24 }}
+            loop={true}
+          />
+        </View>
+      ) : (
+        <KeyboardAwareScrollView
+          contentContainerStyle={{
+            flexGrow: 1,
+            paddingTop: 24,
+            paddingHorizontal: 20,
+            paddingBottom: 80,
+            rowGap: 48,
+          }}
+          keyboardShouldPersistTaps="handled"
+          enableOnAndroid={true}
+          extraScrollHeight={20}
+        >
+          <View className="gap-y-[12px]">
+            <Text className="Semibold16 text-emphasizedFont mx-[4px]">할 일을 입력해주세요</Text>
+            <TextInput
+              value={content}
+              onChangeText={(e) => setContent(e)}
+              placeholder="할 일을 입력해주세요"
+              placeholderTextColor={'#ACADB4'}
+              className={`px-[16px] py-[15px] rounded-xl InputMedium14 text-basicFont h-[49px] border`}
+            />
+          </View>
+
+          <View className="gap-y-[12px]">
+            <Text className="Semibold16 text-emphasizedFont mx-[4px]">담당자를 선택해주세요</Text>
+            <View className="gap-y-[8px]">
+              <View className="flex flex-row flex-wrap gap-[8px]">
+                {memberList.result.mateDetailList.map((item, index) => (
+                  <Pressable
+                    key={index}
+                    onPress={() => toggleSelection(item.mateId)}
+                    className={`px-[20px] py-[10px] rounded-md ${mateIdList.includes(item.mateId) ? 'bg-subColor1' : 'bg-colorBox'}`}
+                  >
+                    <Text
+                      className={`${mateIdList.includes(item.mateId) ? 'Semibold14 text-mainColor' : 'Medium14 text-disabledFont'}`}
+                    >
+                      {item.nickname}
+                    </Text>
+                  </Pressable>
+                ))}
+              </View>
+
+              <View className="flex flex-row items-center">
+                <Pressable onPress={toggleAll} className="p-[8px]">
+                  {mateIdList.length === memberList.result.mateDetailList.length ? (
+                    <CheckBoxIcon />
+                  ) : (
+                    <NotCheckBoxIcon />
+                  )}
+                </Pressable>
+
+                <Text className="Medium14 text-disabledFont">모두</Text>
+              </View>
+            </View>
+          </View>
+
+          <View className="gap-y-[12px]">
+            <Text className="Semibold16 text-emphasizedFont mx-[4px]">날짜를 선택해주세요</Text>
+            <CustomCalendar
+              selectedDate={timePoint}
+              canSelectPrev={false}
+              onDateTimeSelect={(dateTime) => setTimePoint(dateTime)}
+            />
+          </View>
+        </KeyboardAwareScrollView>
+      )}
+
+      <Pressable
+        onPress={() => updateTodo({ content, mateIdList, timePoint })}
+        disabled={!(content !== '' && mateIdList.length > 0 && timePoint !== '')}
+        className={`${
+          content !== '' && mateIdList.length > 0 && timePoint !== ''
+            ? 'bg-mainColor'
+            : 'bg-[#C4C4C4]'
+        }  py-[17.5px] mx-[20px] my-[8px] rounded-xl`}
+      >
+        <Text className="Semibold16 text-white text-center">확인</Text>
+      </Pressable>
+    </View>
+  );
+}
