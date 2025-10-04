5b5ee808d0e499e6fb5498579238b71b5ace39e0
jpark0506 <45231740+jpark0506@users.noreply.github.com>
2025-10-05 00:09:47 +0900

    feat: useGetMyRoomFeed 훅에 roomId 유효성 검사 추가

--- hooks/feed/feed.ts
+++ hooks/feed/feed.ts
@@ -1,7 +1,13 @@
 import { useQuery } from '@tanstack/react-query';
 
 import { queries } from '@/server';
+import { useMemberStore } from '@/zustand/store';
 
 export const useGetMyRoomFeed = () => {
-  return useQuery(queries.feed.detail);
+  const { roomInfo } = useMemberStore();
+
+  return useQuery({
+    ...queries.feed.detail,
+    enabled: roomInfo?.roomId !== undefined,
+  });
 };
