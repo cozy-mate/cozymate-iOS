3f162f74da5afaa96bbc9cebc1b6d0d43fb98117
jpark0506 <45231740+jpark0506@users.noreply.github.com>
2025-10-07 00:53:34 +0900

    feat : text header 공통 컴포넌트 추가

--- components/feed/dormitoryFeed.tsx
+++ components/feed/dormitoryFeed.tsx
@@ -1,5 +1,27 @@
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
+
+import { TextNextHeader } from '../common/textNextHeader';
 
 export default function DormitoryFeed() {
-  return <Text>기숙사 피드</Text>;
+  return <ScrollView className="flex-1 bg-[#F7FAFF] relative pt-6 gap-y-8">
+    <TextNextHeader
+      title={
+        <Text className="Semibold18 text-emphasizedFont">오늘의 메뉴</Text>
+      }
+      handleMore={() => { }}
+    />
+    <TextNextHeader
+      title={
+        <Text className="Semibold18 text-emphasizedFont">공지사항</Text>
+      }
+      handleMore={() => { }}
+    />
+    <TextNextHeader
+      title={
+        <Text className="Semibold18 text-emphasizedFont">기숙사 채팅</Text>
+      }
+      handleMore={() => { }}
+    />
+  </ScrollView>;
 }
