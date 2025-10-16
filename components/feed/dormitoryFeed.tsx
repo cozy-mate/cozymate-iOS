ed2d8dd0087ec06c4f8bf1c635583a48dd3a1bb2
jpark0506 <45231740+jpark0506@users.noreply.github.com>
2025-10-17 00:19:10 +0900

    fix: 필요없는 Param 제거

--- components/feed/dormitoryFeed.tsx
+++ components/feed/dormitoryFeed.tsx
@@ -72,7 +72,7 @@ export default function DormitoryFeed() {
               <View style={{ width: '100%', height: 110, justifyContent: 'center', alignItems: 'center' }}>
                 <MenuItem
                   menuItem={item}
-                  onPress={() => router.push(`/feed/menu?date=${item[0]}`)}
+                  onPress={() => router.push(`/feed/menu`)}
                   isFetching={isMenuFetching}
                 />
               </View>
