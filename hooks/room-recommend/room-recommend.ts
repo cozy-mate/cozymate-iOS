c7ae999fa9c8fdcff817dba8f85d782a4cca93dc
JJHyeok13 <jjinh99@naver.com>
2025-10-18 15:46:16 +0900

    fix: 추천 방 Suspense 제거

--- hooks/room-recommend/room-recommend.ts
+++ hooks/room-recommend/room-recommend.ts
@@ -1,15 +1,14 @@
 import { useInfiniteQuery, useQuery } from '@tanstack/react-query';
 
-import { getRecommendRoomList } from '@/server/room-recommend/room-recommend';
 import { queries } from '@/server';
 import { GetRecommendRoomListResponse } from '@/server/room-recommend/response';
 
 export const useGetHomeRecommendRoomList = () => {
-  return useSuspenseQuery(queries.roomRecommend.home());
+  return useQuery(queries.roomRecommend.home());
 };
 
 export const useGetRecommendRoomList = (getSortType: () => SortTypeValue) => {
-  return useSuspenseInfiniteQuery({
+  return useInfiniteQuery({
     ...queries.roomRecommend.list({ size: 5, sortType: getSortType() }),
     initialPageParam: 0,
     getNextPageParam: (lastPage: GetRecommendRoomListResponse) => {
